c80821c3a288d59fd334e51f1c461f0282d2e79f
Elena Vorontsova <lena.voroncova26@yandex.ru>
2022-11-02 14:31:41 +0400

    add: add mapKit

--- Tinkoff/Tinkoff/Modules/Catering/CateringPresenter.swift
+++ Tinkoff/Tinkoff/Modules/Catering/CateringPresenter.swift
@@ -7,14 +7,51 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 protocol CateringPresenting {
-    func showMap()
+    func checkLocationServices()
+    func checkLocationAuthorization()
+    func showAlert(error: String)
 }
 
 final class CateringPresenter: CateringPresenting {
     weak var controller: CateringViewController?
+    private let locationManager = CLLocationManager()
     
-    func showMap() {
+    func checkLocationServices() {
+        DispatchQueue.global().async {
+            if CLLocationManager.locationServicesEnabled() {
+                self.checkLocationAuthorization()
+            } else {
+                self.showAlert(error: "Проверьте разрешение на использование вашего метсоположения")
+            }
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            controller!.mapView.showsUserLocation = true
+        case .denied:
+            showAlert(error: "Вы запретили использовать ваше местоположение")
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            controller!.mapView.showsUserLocation = true
+        case .restricted:
+            showAlert(error: "Определение местоположения ограничено")
+        case .authorizedAlways:
+            break
+        @unknown default:
+            showAlert(error: "Что-то пошло не так")
+        }
+    }
+    
+    func showAlert(error: String) {
+        let alert = UIAlertController(title: R.string.alertMessages.errorTitle(),
+                                      message: error,
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: R.string.alertMessages.okTitle(), style: .cancel))
+        controller?.present(alert, animated: false)
     }
 }
--- Tinkoff/Tinkoff/Modules/Catering/CateringViewController.swift
+++ Tinkoff/Tinkoff/Modules/Catering/CateringViewController.swift
@@ -7,9 +7,15 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 final class CateringViewController: UIViewController {
+    lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
     private let presenter: CateringPresenting
+    private let locationManager = CLLocationManager()
 
     init(presenter: CateringPresenting) {
         self.presenter = presenter
@@ -19,9 +25,21 @@ final class CateringViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError(R.string.cells.fatalError())
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = R.color.tinkoffGray()
-        presenter.showMap()
+        view.backgroundColor = .white
+        configureConstraints()
+        self.title = "Питание"
+        presenter.checkLocationServices()
+    }
+    
+    private func configureConstraints() {
+        view.addSubview(mapView)
+        mapView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.bottom.equalToSuperview()
+            $0.trailing.leading.equalToSuperview()
+        }
     }
 }
